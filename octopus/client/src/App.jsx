1322099a828861f9ddc295cf24eb1457fb497b99
Isaac Marshall <isaac.marshall@sds.co.uk>
2022-02-03 10:53:03 +0000

    Added initial fetch data functionality (WIP)

--- octopus/client/src/App.jsx
+++ octopus/client/src/App.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client'
+
+const PRODUCTS_LIST = gql`
+     {
+        product(productId: 1) {
+            id
+            name
+            power
+            description
+            price
+            quantity
+            brand
+            weight
+            height
+            width
+            length
+            modelCode
+            colour
+            imgUrl
+          }
+    }
+`
 
 const App = () => {
+    const { loading, error, data } = useQuery(PRODUCTS_LIST);
+
+    if(loading) return <p>Loading...</p>;
+    if(error) return <p>Error</p>;
+
+    console.log('test')
+    console.log(data.product)
     return (
-       <h1>Hello World</h1>
+        <div>
+            <h1>Hello World</h1>
+            {data.product.name}
+
+        </div>
     )
 };
 
--- octopus/client/src/index.jsx
+++ octopus/client/src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: 'http://127.0.0.1:8000/graphql#operationName=getProductById&query=query%20getProductById%20%7B%0A%20%20product(productId%3A%201)%20%7B%0A%20%20%20%20id%0A%20%20%20%20name%0A%20%20%20%20power%0A%20%20%20%20description%0A%20%20%20%20price%0A%20%20%20%20quantity%0A%20%20%20%20brand%0A%20%20%20%20weight%0A%20%20%20%20height%0A%20%20%20%20width%0A%20%20%20%20length%0A%20%20%20%20modelCode%0A%20%20%20%20colour%0A%20%20%20%20imgUrl%0A%20%20%7D%0A%7D%0A',
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
