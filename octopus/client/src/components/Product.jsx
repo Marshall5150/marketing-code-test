e8e3b74de2de8118e32231cfabca79ba93cdd7c9
Isaac Marshall <isaac.marshall@sds.co.uk>
2022-02-04 11:36:46 +0000

    Added comments for missing functionality

--- octopus/client/src/components/Product.jsx
+++ octopus/client/src/components/Product.jsx
@@ -3,9 +3,12 @@ import React, { useState } from "react";
 const Product = (props) => {
     const [count, setCount] = useState(1);
     const [cart, updateCart] = useState([]);
+
+    // Only decreases the quantity if the current value is larger than 1
     const handleMinusClick = () =>
         count > 1 ? setCount(count - 1) : setCount(count - 0);
 
+    // Loops through the quantity and updates array
     const addToBasket = () => {
         for (let i = 0; i < count; i++) {
             updateCart((cart) => [...cart, props.product]);
@@ -20,8 +23,8 @@ const Product = (props) => {
                     src={props.product?.imgUrl}
                     alt={props.product?.name}
                 />
-                <div className="product-block__info">
-                    <div className="product-block__content">
+                <div className="product-block__content">
+                    <div className="product-block__info">
                         <h1 className="mb-1">{props.product?.name}</h1>
                         <p className="font-colour-blue-grey">
                             {props.product?.power} // Packet of {props.product?.quantity}
@@ -31,25 +34,25 @@ const Product = (props) => {
                     <div className="product-block__cta-container background-deep-navy">
                         <div className="flex flex-space-between flex-align-center">
                             <h2>{(props.product?.price / 100).toFixed(2)}</h2>
-                            <div className="flex flex-align-center quantity-block">
+                            <div className="flex flex-align-center">
                                 <button
                                     data-testid="decrementBtn"
                                     onClick={handleMinusClick}
-                                    className="quantity-block__btn"
+                                    className="button button--icon-btn"
                                     aria-label={"Decrease quantity, current value is: " + count}
                                 >
                                     -
                                 </button>
                                 <h3
                                     data-testid="counter"
-                                    className="quantity-block__counter"
+                                    className="ml-1 mr-1"
                                 >
                                     {count}
                                 </h3>
                                 <button
                                     data-testid="incrementBtn"
                                     onClick={() => setCount(count + 1)}
-                                    className="quantity-block__btn"
+                                    className="button button--icon-btn"
                                     aria-label={"Increase quantity, current value is: " + count}
                                 >
                                     +
@@ -66,6 +69,10 @@ const Product = (props) => {
                             Add to cart
                         </button>
 
+                        {/* This is just a mockup to show the items get added to the basket.
+                            Ideally I would have liked to return this data to the parent and then display a message such as a toast
+                            to inform the user how many items had been added to their basket
+                        */}
                         <div data-testid="basket">
                             {cart.length !== 0 ? <p aria-live="assertive" role="alert">You have {cart.length} items in your basket</p> : null}
                         </div>
