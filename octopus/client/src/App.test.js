96242b64015e0bfda3c768d9fb82f9c5b54b2729
Isaac Marshall <isaac.marshall@sds.co.uk>
2022-02-04 10:07:06 +0000

    Created tests

--- octopus/client/src/App.test.js
+++ octopus/client/src/App.test.js
@@ -1,7 +1,42 @@
-test('should be able to increase and decrease product quantity', async () => {
-  expect(true).toBe(false);
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Product from "./components/Product";
+
+let counter;
+let increaseButton;
+let decreaseButton;
+let addToBasketBtn;
+let basket;
+
+beforeEach(() => {
+    render(<Product />);
+
+    increaseButton = screen.getByTestId("incrementBtn");
+    decreaseButton = screen.getByTestId("decrementBtn");
+    counter = screen.getByTestId("counter");
+    addToBasketBtn = screen.getByTestId("addToBasketBtn");
+    basket = screen.getByTestId("basket");
 });
 
-test('should be able to add items to the basket', async () => {
-  expect(true).toBe(false);
+test("should be able to increase and decrease product quantity", async () => {
+    expect(counter).toHaveTextContent("1");
+    userEvent.click(increaseButton);
+    expect(counter).toHaveTextContent("2");
+    userEvent.click(decreaseButton);
+    expect(counter).toHaveTextContent("1");
+});
+
+test("should not be able to decrease product quantity below 1", async () => {
+  expect(counter).toHaveTextContent("1");
+  userEvent.click(decreaseButton);
+  expect(counter).toHaveTextContent("1");
+
+});
+
+test("should be able to add items to the basket", async () => {
+    expect(basket).toBeEmpty();
+    userEvent.click(addToBasketBtn);
+    expect(basket).not.toBeEmpty();
+
 });
